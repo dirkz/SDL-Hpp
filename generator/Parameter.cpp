46e6f092767a0fa75f55979bdac0a03fd9c6ed1a
Dirk Zimmermann <me@dirkz.com>
2025-04-10 17:30:31 +0200

    Have started Parameter, what is needed is its type

--- generator/Parameter.cpp
+++ generator/Parameter.cpp
@@ -3,4 +3,11 @@
 namespace zlang
 {
 
+Parameter::Parameter(CXCursor cursor, CXType cursorType)
+{
+    CXString displayName = clang_getCursorDisplayName(cursor);
+    m_name = clang_getCString(displayName);
+    clang_disposeString(displayName);
 }
+
+} // namespace zlang
--- generator/Parameter.h
+++ generator/Parameter.h
@@ -1,10 +1,18 @@
 #pragma once
 
+#include <string>
+
+#include <clang-c/Index.h>
+
 namespace zlang
 {
 
 struct Parameter
 {
+    Parameter(CXCursor cursor, CXType cursorType);
+
+  private:
+    std::string m_name;
 };
 
 } // namespace zlang
