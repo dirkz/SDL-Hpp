9f48b0a254c44aa3cc02c37ca055c87bcf6b2682
Dirk Zimmermann <me@dirkz.com>
2025-04-10 11:49:51 +0200

    Start parsing, don't have clang linked

--- generator/main.cpp
+++ generator/main.cpp
@@ -1,6 +1,6 @@
-﻿#include <iostream>
+﻿#include <filesystem>
+#include <iostream>
 #include <source_location>
-#include <filesystem>
 
 #include <clang-c/Index.h>
 
@@ -9,7 +9,20 @@ using namespace std;
 int main()
 {
     auto location = source_location::current();
-    cout << "Hello, " << location.file_name() << "\n";
+    auto path =
+        filesystem::path{location.file_name()}.parent_path().parent_path() / "SDL/include/SDL3";
+
+    CXIndex index = clang_createIndex(0, 0);
+
+    CXTranslationUnit unit = clang_parseTranslationUnit(index, path.string().c_str(), nullptr, 0,
+                                                        nullptr, 0, CXTranslationUnit_None);
+
+    if (unit == nullptr)
+    {
+        std::cerr << "Unable to parse translation unit. Quitting.\n";
+        return 0;
+    }
+    CXCursor cursor = clang_getTranslationUnitCursor(unit);
 
     return 0;
 }
